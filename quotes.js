4e5f553269438b91de4a1db7a0c129dc55ea0b5e
unknown <oladejofavourajoke@gmail.com>
2022-04-28 15:16:34 +0100

    first commit

---
+++ quotes.js
@@ -0,0 +1,62 @@
+const QUOTEBANK= [
+    {
+        quote: "The mind is everything. What you think you become.", 
+        author: "~Buddha"
+    },
+    {
+        quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
+        author: "~Chinese Proverb"
+    },
+    {
+        quote: "An unexamined life is not worth living.",
+    author: "~Socrates"
+    },
+    {
+        quote: "Eighty percent of success is showing up.",
+        author: "~Woody Allen"
+    },
+    {
+        quote: "I am not a product of my circumstances. I am a product of my decisions.", 
+        author: "~Stephen Covey"
+    },
+    {
+        quote: "Every child is an artist.  The problem is how to remain an artist once he grows up.", 
+        author: "~Pablo Picasso"
+    },
+    {
+        quote: "You can never cross the ocean until you have the courage to lose sight of the shore.",
+        author: "~Christopher Columbus"
+    },
+    {
+        quote: "Either you run the day, or the day runs you.", 
+        author: "~Jim Rohn"
+    },
+    {
+        quote: "We cannot solve problems with the kind of thinking we employed when we came up with them.",
+        author: "~Albert Einstein"
+    },
+    {
+        quote: "Learn as if you will live forever, live like you will die tomorrow.",
+        author: "~Mahatma Gandhi"
+    },
+    {
+        quote:"When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out. ",
+        author:"~Eleanor Roosevelt"
+    },
+    {
+        quote:"It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest. ",
+        author:"~Walter Anderson"
+    },
+    {
+        quote:"Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together. ",
+        author:"~Diane McLaren"
+    },
+    {
+        quote:"When you change your thoughts, remember to also change your world. ",
+        author:"~Norman Vincent Peale"
+    },
+    {
+        quote: "Make hay while the Sun shines",
+        author: "anonymous"
+    }
+]
